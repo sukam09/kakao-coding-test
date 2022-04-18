003a112670d858571e17546affb3411366d40c14
sukam09 <sukam09@naver.com>
2022-04-18 09:36:52 +0900

    Add 2020 카카오 인턴십/Q1.py

---
+++ 2020 카카오 인턴십/Q1.py
@@ -0,0 +1,41 @@
+def dist(x, y):
+    if y == 0:
+        return push(x, y, 'L')
+    if y == 2:
+        return push(x, y, 'R')
+
+    left_dist = abs(x - left_x) + abs(y - left_y)
+    right_dist = abs(x - right_x) + abs(y - right_y)
+    
+    if left_dist < right_dist:
+        return push(x, y, 'L')
+    elif left_dist > right_dist:
+        return push(x, y, 'R')
+    else:
+        return push(x, y, hand_type)
+
+
+def push(x, y, hand_type):
+    global left_x, left_y, right_x, right_y
+    if hand_type == 'L':
+        left_x, left_y = x, y
+    else:
+        right_x, right_y = x, y
+    return hand_type
+
+
+def solution(numbers, hand):
+    global left_x, left_y, right_x, right_y, hand_type
+    ans = ''
+    left_x, left_y = 3, 0
+    right_x, right_y = 3, 2
+    hand_type = 'L' if hand == 'left' else 'R'
+    
+    for n in numbers:
+        if n == 0:
+            x, y = 3, 1
+        else:
+            x, y = (n - 1) // 3, (n - 1) % 3
+        ans += dist(x, y)
+    
+    return ans
