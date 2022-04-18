4c5033b824f6cad6b1bc16991931c7548d546a20
sukam09 <sukam09@naver.com>
2022-04-18 14:18:16 +0900

    Add 2020 카카오 인턴십/Q2.py

---
+++ 2020 카카오 인턴십/Q2.py
@@ -0,0 +1,52 @@
+from itertools import permutations
+
+
+def calculate(op1, op2, operator):
+    if operator == '+':
+        return op1 + op2
+    elif operator == '-':
+        return op1 - op2
+    else:
+        return op1 * op2
+
+
+def process(expression):
+    stack = []
+    num = ''
+    for e in expression:
+        if e.isdigit():
+            num += e
+        else:
+            stack.append(num)
+            num = ''
+            stack.append(e)
+            operators.add(e)
+    
+    stack.append(num)
+    return stack
+
+
+def solution(expression):
+    global operators
+    operators = set()
+    exp = process(expression)
+    ans = 0
+
+    for p in permutations(operators, len(operators)):
+        new_exp = exp
+        for i in range(len(p)):
+            operator = p[i]
+            stack = []
+
+            for op in new_exp:
+                stack.append(op)
+                if len(stack) >= 3 and stack[-2] == operator:
+                    op2 = stack.pop()
+                    operator = stack.pop()
+                    op1 = stack.pop()
+                    stack.append(calculate(int(op1), int(op2), operator))
+        
+            new_exp = stack
+        ans = max(ans, abs(int(new_exp[0])))
+
+    return ans
