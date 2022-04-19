89f4e890c446a55dfb21d2875857fe5ca884d640
sukam09 <sukam09@naver.com>
2022-04-19 09:28:21 +0900

    Update 2020 카카오 인턴십/Q3.py

--- 2020 카카오 인턴십/Q3.py
+++ 2020 카카오 인턴십/Q3.py
@@ -8,14 +8,16 @@ def solution(gems):
     for start in range(n):
         while end < n and len(cur_gems) < gem_type:
             end += 1
-            if end == n:
-                break
-            if gems[end] in cur_gems:
-                cur_gems[gems[end]] += 1
-            else:
-                cur_gems[gems[end]] = 1
+            if end != n:
+                if gems[end] in cur_gems:
+                    cur_gems[gems[end]] += 1
+                else:
+                    cur_gems[gems[end]] = 1
         
-        if len(cur_gems) == gem_type and end - start + 1 < ans[1] - ans[0] + 1:
+        if end == n:
+            break
+        
+        if end - start + 1 < ans[1] - ans[0] + 1:
             ans = [start + 1, end + 1]
         
         cur_gems[gems[start]] -= 1
