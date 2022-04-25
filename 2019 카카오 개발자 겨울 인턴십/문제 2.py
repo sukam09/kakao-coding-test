a5f79cde37a204883128a518a28860d26c1dbba6
sukam09 <sukam09@naver.com>
2022-04-25 13:49:50 +0900

    Add 2019 카카오 개발자 겨울 인턴십/튜플.py

---
+++ 2019 카카오 개발자 겨울 인턴십/문제 2.py
@@ -0,0 +1,20 @@
+def solution(s):
+    s = s.split('},{')
+    for i in range(len(s)):
+        if '{{' in s[i]:
+            s[i] = s[i].replace('{{', '')
+        if '}}' in s[i]:
+            s[i] = s[i].replace('}}', '')
+        s[i] = s[i].split(',')
+    
+    ans = []
+    checker = {}
+    s = sorted(s, key=lambda x: len(x))
+    for x in s:
+        for y in x:
+            if y not in checker:
+                ans.append(int(y))
+                checker[y] = True
+                break
+
+    return ans
