86a45ffde5d56f5a5b30b51ce7d6bc987e7ef524
sukam09 <sukam09@naver.com>
2022-04-25 09:52:56 +0900

    Add 2019 카카오 개발자 겨울 인턴십/문제 1.py

---
+++ 2019 카카오 개발자 겨울 인턴십/문제 1.py
@@ -0,0 +1,21 @@
+def solution(board, moves):
+    n = len(board)
+    basket = []
+    ans = 0
+
+    for move in moves:
+        j = move - 1
+        for i in range(n):
+            if board[i][j] == 0:
+                continue
+            
+            basket.append(board[i][j])
+            board[i][j] = 0
+            if len(basket) >= 2 and basket[-1] == basket[-2]:
+                basket.pop()
+                basket.pop()
+                ans += 2
+
+            break
+
+    return ans
